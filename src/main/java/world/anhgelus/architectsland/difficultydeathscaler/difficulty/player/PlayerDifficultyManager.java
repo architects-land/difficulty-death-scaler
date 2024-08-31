1d316849d7e66ea595502a2a2757694bdb7324dc
anhgelus <anhgelus.morhtuuzh@proton.me>
2024-08-31 22:41:46 +0000

    feat(difficulty): player difficulty manager

---
+++ src/main/java/world/anhgelus/architectsland/difficultydeathscaler/difficulty/player/PlayerDifficultyManager.java
@@ -0,0 +1,51 @@
+package world.anhgelus.architectsland.difficultydeathscaler.difficulty.player;
+
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.world.Difficulty;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import world.anhgelus.architectsland.difficultydeathscaler.difficulty.DifficultyManager;
+
+public class PlayerDifficultyManager extends DifficultyManager {
+    public final ServerPlayerEntity player;
+
+    public static final int SECONDS_BEFORE_DECREASED = 12*60*60;
+
+    public static final Step[] STEPS = new Step[]{};
+
+    public PlayerDifficultyManager(MinecraftServer server, ServerPlayerEntity player) {
+        super(server);
+        this.player = player;
+        // load saved data
+    }
+
+    @Override
+    protected void onUpdate(UpdateType updateType, Updater updater) {
+
+    }
+
+    @Override
+    protected @NotNull String generateDifficultyUpdate(UpdateType updateType, @Nullable Difficulty difficulty) {
+        return "";
+    }
+
+    @Override
+    protected int getSecondsBeforeDecreased() {
+        return SECONDS_BEFORE_DECREASED;
+    }
+
+    @Override
+    protected Step[] getSteps() {
+        return STEPS;
+    }
+
+    @Override
+    public void applyModifiers(ServerPlayerEntity player) {
+        applyModifiers();
+    }
+
+    public void applyModifiers() {
+
+    }
+}

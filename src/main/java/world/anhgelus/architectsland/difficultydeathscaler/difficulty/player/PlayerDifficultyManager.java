fdbd773c8454162b8b78b9719ef30ddeed173574
anhgelus <anhgelus.morhtuuzh@proton.me>
2024-09-06 13:10:34 +0000

    feat(message): death in day for player difficulty

--- src/main/java/world/anhgelus/architectsland/difficultydeathscaler/difficulty/player/PlayerDifficultyManager.java
+++ src/main/java/world/anhgelus/architectsland/difficultydeathscaler/difficulty/player/PlayerDifficultyManager.java
@@ -140,6 +140,16 @@ public class PlayerDifficultyManager extends DifficultyManager {
         final var sb = new StringBuilder();
         sb.append(generateHeaderUpdate(updateType));
 
+        sb.append("You died: ");
+        if (deathDay >= 4) {
+            sb.append("§2");
+        } else if (deathDay >= 2) {
+            sb.append("§e");
+        } else {
+            sb.append("§c");
+        }
+        sb.append(deathDay).append("§r in less than 24 hours.\n\n");
+
         sb.append("Max hearts: ");
         if (heartAmount == 10) {
             sb.append("§2");

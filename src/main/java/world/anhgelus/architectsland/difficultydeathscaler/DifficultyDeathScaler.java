fb471f0f36066f0c6e15c1b288283a9aeeee72dc
Anhgelus Morhtuuzh <anhgelus@anhgelus.world>
2025-02-18 15:08:19 +0100

    style(config): modify gamerules prefix into dds
    was the modid

--- src/main/java/world/anhgelus/architectsland/difficultydeathscaler/DifficultyDeathScaler.java
+++ src/main/java/world/anhgelus/architectsland/difficultydeathscaler/DifficultyDeathScaler.java
@@ -33,21 +33,22 @@ import java.util.UUID;
 
 public class DifficultyDeathScaler implements ModInitializer {
     public static final String MOD_ID = "difficulty-death-scaler";
+    public static final String GAMERULE_PREFIX = "dds";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     private GlobalDifficultyManager difficultyManager;
 
     public static final GameRules.Key<GameRules.BooleanRule> ENABLE_TEMP_BAN = GameRuleRegistry.register(
-            MOD_ID + ":enableTempBan",
+            GAMERULE_PREFIX + ":enableTempBan",
             GameRules.Category.MISC,
             GameRuleFactory.createBooleanRule(true)
     );
     public static final GameRules.Key<GameRules.IntRule> DEATH_BEFORE_TEMP_BAN = GameRuleRegistry.register(
-            MOD_ID + ":deathBeforeTempBan",
+            GAMERULE_PREFIX + ":deathBeforeTempBan",
             GameRules.Category.MISC,
             GameRuleFactory.createIntRule(5)
     );
     public static final GameRules.Key<GameRules.IntRule> TEMP_BAN_DURATION = GameRuleRegistry.register(
-            MOD_ID + ":tempBanDuration",
+            GAMERULE_PREFIX + ":tempBanDuration",
             GameRules.Category.MISC,
             GameRuleFactory.createIntRule(12)
     );

ab81e654936a6dfe14993d4b1a8e12dd06aab29b
Anhgelus Morhtuuzh <anhgelus@anhgelus.world>
2025-02-20 21:54:46 +0100

    feat(bounty): increase bounty death percentage

--- src/main/java/world/anhgelus/architectsland/difficultydeathscaler/difficulty/player/Bounty.java
+++ src/main/java/world/anhgelus/architectsland/difficultydeathscaler/difficulty/player/Bounty.java
@@ -12,7 +12,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class Bounty extends DifficultyTimer {
-    public static final double BOUNTY_DEATH_PERCENTAGE = 0.02;
+    public static final double BOUNTY_DEATH_PERCENTAGE = 0.04;
     public static final int BOUNTY_ENABLED_AFTER = 20;
 
     private final GlobalDifficultyManager globalDifficulty;
